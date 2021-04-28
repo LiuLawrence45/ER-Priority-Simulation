4da3c8afdab40492418baaa0ef6a0475084aeb92
LiuLawrence45 <liu.lawrence45@gmail.com>
2021-04-28 09:09:44 -0400

    updated from src

--- src/PriorityQueue.java
+++ src/PriorityQueue.java
@@ -3,28 +3,101 @@
  * @author lawrenceliu
  *
  */
-public class PriorityQueue<E> implements PureQueue<E>{
-	E [] data = (E[]) new Object[1000];
 
+import java.util.*;
 
+public class PriorityQueue<E extends Comparable<E>> implements PureQueue<E>{
+	
+	ArrayList<E> queue = new ArrayList<E>();
+	
+	/**
+	 * Dequeue value from root
+	 * @return E
+	 */
 	public E dequeue() {
-		
-		return null;
+		if (queue.size() == 0) {
+			throw new NoSuchElementException("Queue is empty");
+			
+		}
+		else {
+			E returnto = queue.get(0);
+			E temp = queue.get(queue.size() - 1);
+			queue.set(queue.size() - 1, null);
+			queue.set(0,temp);
+			
+			int current = 0;
+			while (queue.get(current).compareTo(getRightVal(current)) > 0
+					|| queue.get(current).compareTo(getLeftVal(current)) > 0) {
+				
+				if (getLeftVal(current).compareTo(getRightVal(current)) < 0) {
+					swap(current, getLeft(current));
+					current = getLeft(current);
+				}
+				else if (getLeftVal(current).compareTo(getRightVal(current)) > 0) {
+					swap(current, getRight(current));
+					current = getRight(current);
+				}
+				
+			}
+			return returnto;
+			
+			
+			
+			
+		}
 	}
+
+	/**
+	 * Swap two indices
+	 * @param one
+	 * @param two
+	 */
+	private void swap(int one, int two) {
+		E temp = queue.get(one);
+		queue.set(one, queue.get(two));
+		queue.set(two, temp);
+	}
+
+	private int getRight(int current) {
+		return (current*2+2);
+	}
+	private int getLeft(int current) {
+		return (current*2+1);
+	}
+	
+	private E getRightVal(int current) {
+		return queue.get(current*2+2);
+	}
+	private E getLeftVal(int current) {
+		return queue.get(current*2+1);
+	}
+	
+	private int getParent(int current) {
+		return current/2;
+	}
+	private E getParentVal(int current) {
+		return queue.get(current/2);
+	}
+
 
 	public void enqueue(E object) {
-		// TODO Auto-generated method stub
+		queue.add(object);
 		
-	}
+		int current = queue.size()-1;
+		while (queue.get(current).compareTo(getParentVal(current)) > 0) {
+			swap(current,getParent(current));
+			current = getParent(current);
+		}
+		
+		
+		}
 
 	public E peek() {
-		// TODO Auto-generated method stub
-		return null;
+		return queue.get(0);
 	}
 
 	public boolean isEmpty() {
-		// TODO Auto-generated method stub
-		return false;
+		return queue.size() == 0;
 	}
 	
 
